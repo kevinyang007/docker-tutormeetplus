05fd265a08d170b92ba98d0d6f31fadb20247e80
TingYao Hsu <tingyaohsu@tutorabc.com>
2016-10-12 17:04:16 +0800

    Update turn entrypoint for LOCAL IP detection

--- entrypoint-turn.sh
+++ entrypoint-turn.sh
@@ -2,7 +2,9 @@
 
 #EXTERNALIP=${EXTERNAL_IP:=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)}
 #EXTERNALIP=${EXTERNAL_IP:=$(curl -s icanhazip.com)}
-LOCALIP=${LOCAL_IP:=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)}
+eth0=$( curl --connect-timeout 3 -s http://169.254.169.254/latest/meta-data/local-ipv4 || ip -4 -o a | grep eth0 | awk {'print $4'} | cut -d/ -f1)
+LOCALIP=${LOCAL_IP:=$eth0}
+
 EXTERNALIP=${LOCALIP}
 
 NAME=${USERNAME:=kurento}
